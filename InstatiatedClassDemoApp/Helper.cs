acf1c37c522b24c6d9d840f756fe34cb031d9cc9
Antonios <antonistsiriplis@hotmail.gr>
2024-02-06 15:16:24 +0100

    example of instatiated class

---
+++ InstatiatedClassDemoApp/Helper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstatiatedClassDemoApp
+{
+    public static class Helper
+    {
+        public static List<PersonModel> SeedData()
+        {
+            List<PersonModel> output = new List<PersonModel>();
+            string firstName = string.Empty;
+
+            do
+            {
+                Console.Write("Entre person's first name:");
+                firstName = Console.ReadLine();
+                if (firstName.ToLower() != "exit")
+                {
+                    PersonModel person = new PersonModel()
+                    {
+                        FirstName = firstName
+                    };
+                    output.Add(person);
+                }
+
+            } while (firstName.ToLower() != "exit");
+
+            return output;
+        }
+    }
+}
---
+++ InstatiatedClassDemoApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace InstatiatedClassDemoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = Helper.SeedData();
+            foreach (var person in people)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
