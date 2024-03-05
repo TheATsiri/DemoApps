7fc05b88af1c51ba73ac19109964f80034dc9500
Antonios <antonistsiriplis@hotmail.gr>
2024-03-05 17:15:42 +0100

    generics demo added

---
+++ GenericsDemoApp/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+List<int> numbers = [1, 2, 3];
+List<string> strings = ["Antonios", "Tsiri", "Polo"];
+
+
+TypeChecker(true);
+TypeChecker(1);
+TypeChecker("Antonios");
+TypeChecker(new PersonRecord("antonios", "tsiriplis"));
+
+void TypeChecker<T>(T value)
+{
+    Console.WriteLine($"Type : {typeof(T)}");
+    Console.WriteLine($"Value : {value}");
+}
+
+record PersonRecord(string FirstName, string LastName);
