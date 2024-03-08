57c749839998551149c39f15886e9a9fd1bb464e
Antonios <antonistsiriplis@hotmail.gr>
2024-03-08 15:19:11 +0100

    add Blazor web app

---
+++ BlazozServerMessageWallApp/Models/MessageModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazozServerMessageWallApp.Models;
+
+public class MessageModel
+{
+    [Required]
+    [StringLength(10, MinimumLength = 5)]
+    public string? Message { get; set; }
+}
---
+++ MVCMessageWallWithIdentifyApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCMessageWallWithIdentifyApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
