889adedcc3b2a2a8fdc71a35f5dba445d7c30784
Antonios <antonistsiriplis@hotmail.gr>
2024-04-30 16:17:51 +0200

    Flutter WebApi app was created

---
+++ FlutterWebApiDemoApp/WebApiApp/WebApi/Models/ApiDBContextModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models
+{
+    public class ApiDBContextModel:DbContext
+    {
+        public ApiDBContextModel(DbContextOptions options):base(options) 
+        {
+            
+        }
+
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
---
+++ FlutterWebApiDemoApp/WebApiApp/WebApi/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        public string? Name { get; set; } = "";
+        [Required]
+        public string? Address { get; set; } = "";
+    }
+}
