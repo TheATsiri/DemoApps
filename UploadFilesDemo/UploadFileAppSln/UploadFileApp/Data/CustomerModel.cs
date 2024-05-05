cfb853b6ea18d97e25840578263fc58d012d77a6
Antonios <antonistsiriplis@hotmail.gr>
2024-05-05 18:58:48 +0200

    more feautures added

---
+++ UploadFilesDemo/UploadFileAppSln/UploadFileApp/Data/CustomerModel.cs
@@ -0,0 +1,10 @@
+﻿namespace UploadFileApp.Data;
+
+public class CustomerModel
+{
+    public int Id { get; set; }
+    public string UserName { get; set; } = "atsiriplis";
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? FileName { get; set; }
+}
