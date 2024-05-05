b782bb745254afae8c34009f50d0ed3f7fe680be
Antonios <antonistsiriplis@hotmail.gr>
2024-05-05 20:08:17 +0200

    talk with SQL

---
+++ UploadFilesDemo/UploadFileAppSln/UploadFileLibrary/SqlDataAccess.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+
+namespace UploadFileLibrary;
+
+public class SqlDataAccess
+{
+    private readonly IConfiguration _config;
+
+    public SqlDataAccess(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public async Task SaveData(
+        string storedProc,
+        string connectionName,
+        object parameters)
+    {
+        string connectionString = _config.GetConnectionString(connectionName);
+    }
+}
